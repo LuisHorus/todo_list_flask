6d4f029678414ce05c52e3f94d55a9e04a797ecd
Horus <luis.escobedo@uinenlinea.mx>
2024-06-16 03:17:44 -0600

    Se crea primera parte del proyecto, plantilla y configuracion Flask

---
+++ todor/__init__.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template
+
+def create_app():
+
+    app = Flask(__name__)
+
+    #Configuracion Proyecto
+    app.config.from_mapping(
+        DEBUG = True,
+        SECRETE_KEY = 'dev'
+    )
+
+    #Blueprint
+    from . import todo
+    app.register_blueprint(todo.bp)
+
+    from . import auth
+    app.register_blueprint(auth.bp)
+
+    @app.route('/')
+
+    def index():
+        return render_template('index.html')
+
+    return app
